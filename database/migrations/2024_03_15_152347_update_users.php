0ba53bb205606d8670cc2ead2aa4b19d8b7c0c5b
mishkin02 <mishkin_ii@edu.surgu.ru>
2024-03-15 21:16:46 +0500

    Migrations task

---
+++ database/migrations/2024_03_15_152347_update_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name', 32)->nullable();
+            $table->string('last_name', 32)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('phone_number', 18)->nullable();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('first_name', 32);
+                $table->dropColumn('last_name', 32);
+                $table->dropColumn('email', 100);
+                $table->dropColumn('phone_number', 18);
+            });
+    }
+};
